99550baf8bcef67a1f3a3f17c840be7c1ffcc9c7
Keshen <njkeshen98@gmail.com>
2024-11-11 14:30:01 +0800

    Destructured the props property

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,6 @@
 import Student from "./student";
 
-function StudentsList(props) {
+function StudentsList() {
   return (
     <div
       className="card rounded shadow-sm mx-auto my-4"
@@ -13,6 +13,7 @@ function StudentsList(props) {
         <Student num={1} name="John" />
         <Student num={2} name="Jane" />
         <Student num={3} name="Jack" />
+        <Student />
       </div>
     </div>
   );
--- src/components/student.js
+++ src/components/student.js
@@ -2,12 +2,16 @@
 // props passes data from parent code to child code
 
 function Student(props) {
+    const { num = 0, name = "No name" } = props; // Destructuring
     return (
-        <div className="d-flex justify-content-between align-items-center mt-3">
-            <h5 className="mb-0">{props.num}. {props.name}</h5>
-        </div>
+      <div className="d-flex justify-content-between align-items-center mt-3">
+        <h5 className="mb-0">
+          {num}. {name}
+        </h5>
+      </div>
     );
 }
-
+  
 export default Student;
+  
   
