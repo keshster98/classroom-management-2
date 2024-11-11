bef67633c04c880e38f47a53c1acd494594b2c36
Keshen <njkeshen98@gmail.com>
2024-11-11 14:24:09 +0800

    Enabled the props property to manage the static data of students

--- src/components/list.js
+++ src/components/list.js
@@ -1,21 +1,21 @@
 import Student from "./student";
 
-function StudentsList() {
-    return (
-      <div
-        className="card rounded shadow-sm mx-auto my-4"
-        style={{
-          maxWidth: "500px",
-        }}
-      >
-        <div className="card-body">
-          <h3 className="card-title mb-3">Students</h3>
-          <Student />
-          <Student />
-        </div>
+function StudentsList(props) {
+  return (
+    <div
+      className="card rounded shadow-sm mx-auto my-4"
+      style={{
+        maxWidth: "500px",
+      }}
+    >
+      <div className="card-body">
+        <h3 className="card-title mb-3">Students</h3>
+        <Student num={1} name="John" />
+        <Student num={2} name="Jane" />
+        <Student num={3} name="Jack" />
       </div>
-    );
+    </div>
+  );
 }
-  
+
 export default StudentsList;
-  
--- src/components/student.js
+++ src/components/student.js
@@ -1,10 +1,13 @@
-function Student() {
+// "props" exists in every React Component
+// props passes data from parent code to child code
+
+function Student(props) {
     return (
-      <div className="d-flex justify-content-between align-items-center mt-3">
-        <h5 className="mb-0">1. John</h5>
-      </div>
+        <div className="d-flex justify-content-between align-items-center mt-3">
+            <h5 className="mb-0">{props.num}. {props.name}</h5>
+        </div>
     );
-  }
-  
-  export default Student;
+}
+
+export default Student;
   
